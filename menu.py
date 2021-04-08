e171383d36fb616b58cd18a673f7582e6677b6f4
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-04-08 12:31:12 +0800

    4(add read, continue)

--- menu.py
+++ menu.py
@@ -5,26 +5,28 @@ Created on Tue Mar 30 10:43:37 2021
 
 @author: yuanke.tsai
 """
-#新增商品陳列
-#新增寫入csv檔
+#新增讀取檔案、跳過欄位讀取(L66)
+
+
+#檔案讀取
 products = []
+with open('menu_write.csv', 'r', encoding = 'utf-8') as file:
+    for line in file:
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print('先前購買物品清單：', '\n', products)
+
+#使用者輸入
 while True:
     name = input('請輸入購買的商品（q/離開）：')
     if name == 'q':
         break
     price = input('請輸入商品價個：')
-    #solution 1
-    #product = []
-    #product.append(name)
-    #product.append(price)
-    
-    #solution 2
-    #product = [name, price]
-    #products.append(product)
-    
-    #solution 3
-    products.append([name, price])
-    
+    products.append([name, price]) #此時清單products = '舊資料 + 新輸入'
+
+#顯示過去已購買物品    
 i = 0
 r = 1
 while True:
@@ -35,8 +37,8 @@ while True:
     else:
         break
 
-#亦或 with open('menu_write.csv', 'w', encoding = 'utf-8') as file:
-with open('menu_write.csv', 'w') as file:
+#新清單 producs 做寫入
+with open('menu_write.csv', 'w', encoding = 'utf-8') as file:
     file.write('商品,價格' + '\n')
     for product in products:
         file.write(product[0] + ',' + product[1] + '\n')
