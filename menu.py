13d13e39864b344ecf9e3570deb73eeaf9d3d56b
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-04-16 11:44:19 +0800

    5(add file check)

--- menu.py
+++ menu.py
@@ -5,20 +5,25 @@ Created on Tue Mar 30 10:43:37 2021
 
 @author: yuanke.tsai
 """
-#新增讀取檔案、跳過欄位讀取(L66)
+#為避免第一段因沒有檔案而進行不下去做修改 （L67）
 
+import os
 
-#檔案讀取
+#第一段：檔案讀取，並檢查使否存在可讀取
 products = []
-with open('menu_write.csv', 'r', encoding = 'utf-8') as file:
-    for line in file:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print('先前購買物品清單：', '\n', products)
+if os.path.isfile('menu_write.csv'):
+    print('檔案存在，執行中', '\n')
+    with open('menu_write.csv', 'r', encoding = 'utf-8') as file:
+        for line in file:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print('先前購買物品清單：', '\n', products)
+else:
+    print('檔案不存在，請進行以下建立檔案')
 
-#使用者輸入
+#第二段：使用者輸入
 while True:
     name = input('請輸入購買的商品（q/離開）：')
     if name == 'q':
@@ -26,7 +31,7 @@ while True:
     price = input('請輸入商品價個：')
     products.append([name, price]) #此時清單products = '舊資料 + 新輸入'
 
-#顯示過去已購買物品    
+#第三段：顯示過去已購買物品    
 i = 0
 r = 1
 while True:
@@ -37,10 +42,11 @@ while True:
     else:
         break
 
-#新清單 producs 做寫入
+#第四段：新清單 producs 做寫入
 with open('menu_write.csv', 'w', encoding = 'utf-8') as file:
     file.write('商品,價格' + '\n')
     for product in products:
         file.write(product[0] + ',' + product[1] + '\n')
+print(products)
     
     
